5f9cc8aa189364e6106da236847aa17cc7fad474
Yoav Shati <40692715+yoavshati@users.noreply.github.com>
2023-11-24 10:34:42 +0200

    Update tailwind.config.cjs: improve height on mobile
    add dvh unit with vh fallback

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,7 +15,10 @@ module.exports = {
         "2xl": "1400px",
       },
     },
-    extend: {
+    extend: {   
+      height: {
+        screen: ['100vh /* fallback for Opera, IE and etc. */', '100dvh'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
